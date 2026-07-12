ab14090d4d8296c4a800c5496b332acbb60e90c5
lautiiipks <ibanezlautaro21@gmail.com>
2026-07-12 18:10:53 -0300

    eliminaciond de ramales en recorridos

--- src/constants/recorridos.js
+++ src/constants/recorridos.js
@@ -16,49 +16,41 @@ export const recorridos = [
   {
     id: 1,
     nombre: "Buenos Aires - Mar del Plata",
-    descripcion: "Ramal 1",
     imagen: LOGO_TREN,
   },
   {
     id: 2,
     nombre: "Buenos Aires - Gral. Guido - Divisadero de Pinamar",
-    descripcion: "Ramal 2",
     imagen: LOGO_TREN,
   },
   {
     id: 3,
     nombre: "Buenos Aires - Bragado",
-    descripcion: "Ramal 3",
     imagen: LOGO_TREN,
   },
   {
     id: 4,
     nombre: "Buenos Aires - Junín",
-    descripcion: "Ramal 4",
     imagen: LOGO_TREN,
   },
   {
     id: 5,
     nombre: "Buenos Aires - Justo Daract",
-    descripcion: "Ramal 5",
     imagen: LOGO_TREN,
   },
   {
     id: 6,
     nombre: "Buenos Aires - Rosario",
-    descripcion: "Ramal 6",
     imagen: LOGO_TREN,
   },
   {
     id: 7,
     nombre: "Buenos Aires - Córdoba",
-    descripcion: "Ramal 7",
     imagen: LOGO_TREN,
   },
   {
     id: 8,
     nombre: "Buenos Aires - Tucumán",
-    descripcion: "Ramal 8",
     imagen: LOGO_TREN,
   },
 ];
